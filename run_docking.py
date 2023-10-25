0307b17b24870dfcee941e3131bcd07bd567152b
David Hoksza <david.hoksza@gmail.com>
2023-10-25 11:15:27 +0200

    code cleanup
    improved (hopefully :) ) readme

--- run_docking.py
+++ run_docking.py
@@ -3,14 +3,14 @@ import subprocess
 import json
 import argparse
 import sys
-import zipfile ##### FIXED
+import zipfile
 import shutil
 
 MGLTOOLS_LIB_PATH = '/opt/mgltools_x86_64Linux2_1.5.7/MGLToolsPckgs'
 os.environ['PYTHONPATH'] = '/opt/mgltools_x86_64Linux2_1.5.7/MGLToolsPckgs'
 MGLTOOLS_BIN_PATH = '/opt/mgltools_x86_64Linux2_1.5.7/MGLToolsPckgs/AutoDockTools/Utilities24/'
-DATA_PATH = "/data/" ##### FIXED
-WORK_DIR ="/tmp"
+DATA_PATH = "/data/"
+WORK_PATH ="/tmp"
 
 def filter_pdb(input_pdb, output_pdb):
     with open(input_pdb, 'r') as f_in, open(output_pdb, 'w') as f_out:
@@ -27,7 +27,7 @@ def prepare_receptor(pdb_file, pdbqt_file):
 def prepare_ligand(input_file, output_file):
     cmd = ["/usr/local/bin/python2.7", os.path.join(MGLTOOLS_BIN_PATH, 'prepare_ligand4.py'), '-l', input_file, '-o', output_file]
     current_directory = os.getcwd()
-    os.chdir(WORK_DIR) ####FIXED
+    os.chdir(WORK_PATH)
     try:
         subprocess.run(cmd, check=True)
     finally:
@@ -35,15 +35,10 @@ def prepare_ligand(input_file, output_file):
 
 def convert_to_pdb(input_file, output_file):
     _, ext = os.path.splitext(input_file)
-    ##### FIX
-    if ext.lower() == '.smi':
-        input_format = 'smi'
-    elif ext.lower() == '.mol2':
-        input_format = 'mol2'
-    elif ext.lower() == '.cif':
-        input_format = 'cif'
-    elif ext.lower() == '.sdf':
-        input_format = 'sdf'
+    
+    ext_lower = ext.lower()
+    if ext_lower in ['.smi', '.mol2', '.cif', '.sdf']:
+        input_format = ext_lower[1:]
     else:
         raise ValueError(f"Unsupported input format for conversion: {ext}")
     cmd = ["obabel", "-i", input_format, input_file, "-o", "pdb", "-O", output_file, "--gen3d"]
@@ -56,7 +51,7 @@ def read_json(json_file):
     
     receptor = os.path.join(DATA_PATH, data["receptor"])
     ligand = os.path.join(DATA_PATH, data["ligand"])
-    output = os.path.join(WORK_DIR, data["output"])
+    output = os.path.join(WORK_PATH, data["output"])
     
     center_x = data["center"]["x"]
     center_y = data["center"]["y"]
@@ -81,27 +76,23 @@ def create_zip(output_folder, files, zip_filename):
 
 
 def main(json_file):
-
-    ###FIXED? ... os.makedirs(output_folder, exist_ok=True)
-    os.makedirs("/data/output", exist_ok=True)
-    os.chmod("/data/output", 0o777)
-
+    
     receptor, ligand, output, center_x, center_y, center_z, size_x, size_y, size_z = read_json(json_file)
 
     receptor = check_file_exists(receptor)
     ligand = check_file_exists(ligand)
-
-    # Convert receptor file to file with aminoacid sequence format only
-    _, tail = os.path.split(receptor)
-    receptor_aux = f'{WORK_DIR}/{tail}'
-    shutil.copy2(receptor, receptor_aux)
-    filtered_receptor = receptor_aux.replace(".pdb", "_filtered.pdb")
-    filter_pdb(receptor_aux, filtered_receptor) 
-    receptor = filtered_receptor ### FIX
-
-    # Convert receptor if it's not in pdbqt format   ####FIX? before we worked with it as if was pdb file for sure
+    
+    # Convert receptor if it's not in pdbqt format
     base, ext = os.path.splitext(receptor)
     if ext.lower() == '.pdb':
+        # Convert receptor file to file with aminoacid sequence format only
+        _, tail = os.path.split(receptor)
+        receptor_aux = f'{WORK_PATH}/{tail}'
+        shutil.copy2(receptor, receptor_aux)
+        filtered_receptor = receptor_aux.replace(".pdb", "_filtered.pdb")
+        filter_pdb(receptor_aux, filtered_receptor) 
+        receptor = filtered_receptor
+
         receptor_pdbqt = os.path.abspath(base + '.pdbqt')
         prepare_receptor(receptor, receptor_pdbqt)
         check_file_exists(receptor_pdbqt)
@@ -114,11 +105,11 @@ def main(json_file):
     ext_lower = ext.lower()
     if ext_lower in ['.smi', '.mol2', '.cif', '.sdf', '.pdb']:
         if ext_lower != '.pdb':
-            ligand_pdb = os.path.join(WORK_DIR, tail.replace(ext, '.pdb'))
+            ligand_pdb = os.path.join(WORK_PATH, tail.replace(ext, '.pdb'))
             print(f"Converting {ligand} to {ligand_pdb} ...")
             convert_to_pdb(ligand, ligand_pdb)
             check_file_exists(ligand_pdb)
-        ligand_pdbqt = os.path.join(WORK_DIR, tail.replace(ext, '.pdbqt'))
+        ligand_pdbqt = os.path.join(WORK_PATH, tail.replace(ext, '.pdbqt'))
         #ligand_pdbqt = os.path.abspath(f'{WORK_DIR}{base}.pdbqt')
         print(f"Converting {ligand} to {ligand_pdbqt} ...")
         prepare_ligand(ligand_pdb, ligand_pdbqt)
